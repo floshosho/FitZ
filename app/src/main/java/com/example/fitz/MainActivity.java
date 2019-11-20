8426e9e3f339106ef6c858c8c70bc6c8978bdd04
floshosho <fshosho02151@gmail.com>
2019-11-19 20:48:27 -0500

    firebase init

--- app/src/main/java/com/example/fitz/MainActivity.java
+++ app/src/main/java/com/example/fitz/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.fitz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,13 +19,16 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i(TAG, "Login button pressed");
                 Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_SHORT).show();
             }
         });
+
         Button createAccount = (Button) findViewById(R.id.button_create_account);
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i(TAG, "Create Account button pressed");
                 Toast.makeText(getApplicationContext(), "Create Account", Toast.LENGTH_SHORT).show();
             }
         });
