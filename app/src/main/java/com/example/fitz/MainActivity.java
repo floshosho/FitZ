690da8eb688a62737cd7abd5ad0e54316c361158
floshosho <fshosho02151@gmail.com>
2019-11-19 19:44:27 -0500

    Added initial login screen, still not done with creating account/authentication

--- app/src/main/java/com/example/fitz/MainActivity.java
+++ app/src/main/java/com/example/fitz/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.fitz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button login = (Button) findViewById(R.id.button_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button createAccount = (Button) findViewById(R.id.button_create_account);
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Create Account", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
 }
