eb95d263d00403c44aab6de2cfb9f08e073f5607
floshosho <fshosho02151@gmail.com>
2019-11-20 20:28:50 -0500

    Firebase login and account creation now work. Full implementation will be finished once we add a main menu

---
+++ app/src/main/java/com/example/fitz/CreateAccountActivity.java
@@ -0,0 +1,90 @@
+package com.example.fitz;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    private final static String TAG = CreateAccountActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
+
+    // TODO Start this when we add the main menu
+    // When main activity starts, check if user is currently signed in
+    // if so, then redirect to main menu activity, otherwise goto sign in activity
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        // update UI
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        mAuth = FirebaseAuth.getInstance();
+        Button register = (Button) findViewById(R.id.button_create_register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText email = (EditText) findViewById(R.id.editText_email2);
+                EditText password = (EditText) findViewById(R.id.editText_password2);
+                EditText confPassword = (EditText) findViewById(R.id.editText_confirm_password);
+                createAccount(email.getText().toString(), password.getText().toString(),
+                        confPassword.getText().toString());
+            }
+        });
+    }
+
+    private void createAccount(String email, String password, String confPassword) {
+        try {
+            // Make sure credentials a
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "One or more fields are empty", Toast.LENGTH_SHORT).show();
+                throw new FirebaseAuthInvalidCredentialsException(TAG, "One or more fields are empty or null");
+            }
+            if (password.length() < 6) {
+                Toast.makeText(getApplicationContext(), "Your password is too short", Toast.LENGTH_SHORT).show();
+                throw new FirebaseAuthInvalidCredentialsException(TAG, "Password < 6 characters");
+            }
+            if (!password.equals(confPassword)) {
+                Toast.makeText(getApplicationContext(), "Your passwords don't match", Toast.LENGTH_SHORT).show();
+                throw new FirebaseAuthInvalidCredentialsException(TAG, "Password strings are not equal");
+            }
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(getApplicationContext(), "Welcome to FitZ!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.d(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        } catch (Exception e) {
+            Log.i(TAG, e.getStackTrace().toString());
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/fitz/SignInActivity.java
@@ -0,0 +1,93 @@
+package com.example.fitz;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private final static String TAG = SignInActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
+    private final boolean debug = false;
+
+    // TODO Start this when we add the main menu
+    // When main activity starts, check if user is currently signed in
+    // if so, then redirect to main menu activity, otherwise goto sign in activity
+//    @Override
+//    public void onStart() {
+//        // Handle error cases here eventually
+//        super.onStart();
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        mAuth = FirebaseAuth.getInstance();
+
+        Button login = (Button) findViewById(R.id.button_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText email = (EditText) findViewById(R.id.editText_email); // get email from EditText obj
+                EditText password = (EditText) findViewById(R.id.editText_password); // get password from EditText obj
+                signInUser(email.getText().toString(), password.getText().toString()); // Attempt to sign in the user
+            }
+        });
+
+        Button createAccount = (Button) findViewById(R.id.button_create_account);
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Create Account button pressed");
+                Intent intent = new Intent(getApplicationContext(), CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void signInUser(String email, String password) {
+        try {
+            // Make sure credentials a
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                throw new FirebaseAuthInvalidCredentialsException(TAG, "One or both credentials is empty or null");
+            }
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.d(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        } catch (Exception e) {
+            Log.i(TAG, e.getStackTrace().toString());
+        }
+    }
+
+}
