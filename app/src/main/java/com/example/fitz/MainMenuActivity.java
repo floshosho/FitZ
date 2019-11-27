8a9a55c2d534f6a65d0d562f748190100e841c89
SMony-L <l.sovanmony@gmail.com>
2019-11-26 23:21:28 -0500

    Google Api and Profile Setting

--- app/src/main/java/com/example/fitz/MainMenuActivity.java
+++ app/src/main/java/com/example/fitz/MainMenuActivity.java
@@ -1,8 +1,13 @@
 package com.example.fitz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,5 +18,30 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.account_circle:
+                            selectedFragment = new Account();
+                            break;
+                        case R.id.fitness_center:
+                            selectedFragment = new StrengthExercise();
+                            break;
+                        case R.id.routine:
+                            break;
+                        case R.id.map:
+                            selectedFragment = new MyMapsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
 }
--- app/src/main/java/com/example/fitz/MyMapsFragment.java
+++ app/src/main/java/com/example/fitz/MyMapsFragment.java
@@ -3,29 +3,53 @@ package com.example.fitz;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MyMapsFragment extends Fragment {
+public class MyMapsFragment extends Fragment implements OnMapReadyCallback {
 
-
+    GoogleMap map;
     public MyMapsFragment() {
         // Required empty public constructor
     }
 
-
+    SupportMapFragment mapFragment;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_maps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map1);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng BSC = new LatLng(42.346653, -71.080825);
+        map.addMarker(new MarkerOptions().position(BSC).title("Boston Sport Club"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(BSC));
     }
 
 }
